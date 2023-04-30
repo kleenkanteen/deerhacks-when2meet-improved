c8dc46cd282f31fa31bef8bd5da39f4203efe923
David Kochanski <105320855+davefoxxo@users.noreply.github.com>
2023-04-29 21:59:24 -0400

    Debugging

--- selectCalendar.js
+++ selectCalendar.js
@@ -17,7 +17,9 @@ let supabaseClient = await initSupabase();
 //   console.log(data, error);
 
 let outDays;
-
+let outTimes;
+let didSelectedTimesChange = false;
+let values = []; // This will store all the selected times
 
 const columns = document.querySelectorAll(".day");
 let dayStates = Array(7).fill(false);
@@ -49,7 +51,6 @@ async function formatTime(num) {
     let minutes = num % 100;
     return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
 }
-
 
 
 const createEventbtn = document.getElementById("create-event-btn");
@@ -88,7 +89,7 @@ createEventbtn.onclick = async (e) => {
     let fromIdx = times.indexOf(fromTimeFormatted);
     let toIdx = times.indexOf(toTimeFormatted);
 
-    let outTimes = times.slice(fromIdx, toIdx + 1);
+    outTimes = times.slice(fromIdx, toIdx + 1);
     outDays = [];
 
     dayStates.forEach((day, i) => {
@@ -121,10 +122,13 @@ createEventbtn.onclick = async (e) => {
     document.getElementById("setup").style.display = "none";
 }
 
+const updateSelectedTimes = (event) => {
+
+}
+
 
 const nameBtn = document.getElementById("auth-submit");
 
-let values;
 let username;
 nameBtn.onclick = async (e) => {
     e.preventDefault();
@@ -143,17 +147,21 @@ nameBtn.onclick = async (e) => {
     // Account for half hours being kinda weird
     const userCalendar = document.getElementById("user-calendar");
     let atHalfHour = fromTime.toString().charAt(fromTime.toString().length - 2) == "3";
-    let numRows;
 
-    if(atHalfHour) {
-        numRows = 1 + (toTime - fromTime + 70) / 50;
-    } else {
-        numRows = (toTime - fromTime + 30) / 30;
-    }
+    const times = ['08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00'];
+
+    let fromTimeFormatted = await formatTime(fromTime);
+    let toTimeFormatted = await formatTime(toTime);
+
+    let fromIdx = times.indexOf(fromTimeFormatted);
+    let toIdx = times.indexOf(toTimeFormatted);
+
+    let numRows = toIdx - fromIdx + 1;
+
 
     let numCols = 0;
 
-    // CONSTRUCT THE CALENDAR based on number of rows, cols, times, etc.
+    // CONSTRUCT THE CALENDAR topmost light blue headers based on number of days
     let out = `<div class='day-header'></div>`
     for(let i = 0; i < 7; i++) {
         if(!dayStates[i]) continue;
@@ -164,7 +172,7 @@ nameBtn.onclick = async (e) => {
 
     let currTime = fromTime;
 
-    // Times (left most column)
+    // Times (left most column light blue)
     out += "<div>";
     for(let i = 0; i < numRows; i++) {
         let s = currTime.toString().slice(0, -2) + ":" + currTime.toString().slice(-2);
@@ -180,7 +188,7 @@ nameBtn.onclick = async (e) => {
 
     out += "</div>";
 
-    // The actual days and selectable cells
+    // The actual clickable times of day
     for(let i = 0; i < 8; i++) {
 
         if(!dayStates[i]) continue;
@@ -194,23 +202,37 @@ nameBtn.onclick = async (e) => {
 
     }
 
+
     // Update them
     userCalendar.style.gridTemplateColumns = `1fr repeat(${numCols}, 2fr)`;
     userCalendar.innerHTML = out;
 
+    const cols = document.querySelectorAll("#user-calendar .cell-col");
+    cols.forEach((col) => {
+        values.push([Array(col.children.length).fill(false)]);
+    })
+
+    console.log("Values initialized as, ", values);
+
+        // WIP RESET BUTTON
+    // document.getElementById("reset-calendar").onclick = (event) => {
+    //     event.preventDefault();
+    //     values = [];
+    
+    //     cols.forEach((col) => {
+    //         values.push([Array(col.children.length).fill(false)]);
+
+    //     })
+
+    // }
 
     document.querySelector(".user-calendar-settings").style.display = "block";
 
     // userCalendarSettings
 
     // DRAGGING AND SELECTING TIMES
-    const cols = document.querySelectorAll("#user-calendar .cell-col");
-    values = []; // This will store all the highlighted stuff
+    let selectedTimes = [];
     let isDragging = false;
-
-    cols.forEach((col) => {
-        values.push([Array(col.children.length).fill(false)]);
-    })
 
     let tabs = [];
 
@@ -225,17 +247,7 @@ nameBtn.onclick = async (e) => {
 
     });
 
-    // WIP RESET BUTTON
-    // document.getElementById("reset-calendar").onclick = (event) => {
-    //     event.preventDefault();
-    //     values = [];
-    
-    //     cols.forEach((col) => {
-    //         values.push([Array(col.children.length).fill(false)]);
 
-    //     })
-
-    // }
 
     onmousedown = () => {
         isDragging = true;
@@ -243,22 +255,93 @@ nameBtn.onclick = async (e) => {
 
     onmouseup = () => {
         isDragging = false;
+        didSelectedTimesChange = true;
     }
 
+    // {"M": [{900: true, 930: true., ,}]}
+    let cloned;
     tabs.forEach((col, i) => {
         col.forEach((tab, j) => {
             tab.addEventListener("mouseenter", (event) => {
                 if(!isDragging) return;
                 tab.classList.toggle("selected");
+                console.log(`changing values index i =${i} index y = ${j}`)
+                cloned = values[i];
+                cloned[j] = !cloned[j];
+                console.log("cloned", cloned.length, cloned);
                 values[i][j] = !values[i][j];
+                console.log("values updated as ,", values);
             })
 
             tab.addEventListener("mousedown", (event) => {
                 tab.classList.toggle("selected")
-                values[i][j] = !values[i][j];
+                console.log(`changing values index i =${i} index y = ${j}`)
+                cloned = values[i];
+                cloned[j] = !cloned[j];
+                console.log("cloned", cloned.length, cloned);
+                values[i][j] = values[i][j];
+                console.log("values updated as ,", values);
+                
             })
 
         })
     })
+
+
+    
+    const groupCalendar = document.getElementById("group-calendar");
+
+    groupCalendar.style.gridTemplateColumns = `1fr repeat(${numCols}, 2fr)`;
+
+    groupCalendar.innerHTML = out;
+
+//              M            Friday
+// values = [[t, t, f], [t, f, f]...]
+
+
+
+
+
+    setInterval(async () => {
+        if(didSelectedTimesChange) {
+            values.forEach((day, i) => {
+                day.forEach((time, j) => {
+                    if(time) {
+                        selectedTimes.push({Day: outDays[i], Time: outTimes[j], Event: eventName, UserID: username});
+                    }
+                })
+            })
+
+
+            console.log(selectedTimes);
+            const { data1, error1 } = ( await supabaseClient
+                .from('Times')
+                .insert(
+                  selectedTimes,
+                ));
+            console.log(data1, error1)
+        
+        
+        
+            let AvailableCounter = {}
+            outDays.forEach((day) => {
+                AvailableCounter[day] = {};
+                outTimes.forEach((time) => {
+                    AvailableCounter[day][time] = [] // Use day instead of "day"
+                })
+            });
+        
+            const { data, error } = await supabaseClient
+            .from('Times')
+            .select()
+            .not('UserID', 'is', null)
+            console.log('read combined times', data, error)
+        
+
+            didSelectedTimesChange = false;
+        }
+
+
+    }, 1000) 
 }
 
